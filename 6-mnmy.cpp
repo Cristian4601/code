b410f6a141ab082ecf818fab79a054fbcf447e91
Cristian4601 <cristianmorenoromero46@gmail.com>
2026-09-12 15:22:16 -0500

    add test

---
+++ 6-mnmy.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ctype.h>
+using namespace std;
+
+int main()
+{
+    int Opc;
+    char Fras[20];
+
+    cout<<"ingrese una frase: ";
+    cin>>Fras;
+
+    cout<<"1- mayusculas \n2- minusculas \n---->";
+
+}
