39938bc369b1b015dcd6520fadb7dcf4ad6ae66e
Cristian4601 <cristianmorenoromero46@gmail.com>
2026-09-12 15:36:25 -0500

    finish (6)

--- 6-mnmy.cpp
+++ 6-mnmy.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
+#include <string.h>
 #include <ctype.h>
+
 using namespace std;
 
-int main()
-{
-    int Opc;
-    char Fras[20];
+int main() {
+    string texto;
+    int i;
 
-    cout<<"ingrese una frase: ";
-    cin>>Fras;
+    cout << "Ingrese una cadena: ";
+    getline(cin, texto);
 
-    cout<<"1- mayusculas \n2- minusculas \n---->";
+    string mayusculas = texto;
+    string minusculas = texto;
 
+    for (i=0;i<mayusculas.length();i++) {
+        mayusculas[i] = toupper(mayusculas[i]);
+    }
+
+    for (i=0;i<minusculas.length();i++) {
+        minusculas[i] = tolower(minusculas[i]);
+    }
+
+    cout << "\nTexto original: " << texto << endl;
+    cout << "En mayusculas: " << mayusculas << endl;
+    cout << "En minusculas: " << minusculas << endl;
+
+    return 0;
 }
